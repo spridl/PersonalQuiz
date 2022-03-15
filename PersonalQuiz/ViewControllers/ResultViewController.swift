e85de7d3e59aa4ff09ad80af6eb7703650d7a9ba
Tim <spridl@mail.ru>
2022-03-16 01:56:22 +0300

    config ResultViewController

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -9,35 +9,39 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
-    // 4. Избавиться от кнопки возврата назад на экране результатов
-
     @IBOutlet weak var animalLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
     var answers: [Answer]!
     
-    private var yourAnimal: Animal!
+    var yourAnimal: Character = "a"
+    var definition = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
         setTypeAnimal()
-        animalLabel.text = "ВЫ \(yourAnimal.rawValue)"
-        descriptionLabel.text = yourAnimal.definition
+        animalLabel.text = "ВЫ \(yourAnimal)"
+        descriptionLabel.text = definition
     }
     
     private func setTypeAnimal() {
         var animals = [Animal]()
+        
         for answer in answers {
             animals.append(answer.animal)
         }
-        let animalsMatchCount = animals.reduce(into: [:]) { counts, letter in
-            counts[letter, default: 0] += 1
+        
+        let animalsMatchCount = animals.reduce(into: [:]) { counts, animal in
+            counts[animal, default: 0] += 1
         }
+        
         var greatestMatchCount = 0
         for animal in animalsMatchCount {
             if animal.value > greatestMatchCount {
                 greatestMatchCount = animal.value
-                yourAnimal = animal.key
+                yourAnimal = animal.key.rawValue
+                definition = animal.key.definition
             }
         }
     }
